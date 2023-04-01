fa14db90a14ba8064806cd5d64e02544a55e4217
Róbert Fodor <fodor.robert@gmail.com>
2023-04-01 12:35:52 +0200

    regression model and model change added

--- modules/regression.R
+++ modules/regression.R
@@ -6,7 +6,6 @@ library(ggplot2) # for plotting
 library(shinyWidgets) # for custom widgets
 library(car) # vif
 library(lm.beta) # standardized beta coefficients
-library(psych) # needed for corr.test
 
 # UI
 regression_ui <- function(id) {
@@ -50,7 +49,6 @@ regression_ui <- function(id) {
             fluidRow(
                 column(
                     width = 12,
-                    verbatimTextOutput(ns("debug")),
                     h3("Model Fit Measures"),
                     tableOutput(ns("model_fit_measures")),
                     h3("Model Change Measures"),
@@ -58,7 +56,8 @@ regression_ui <- function(id) {
                     h3("Model Coefficients"),
                     verbatimTextOutput(ns("model_coefficients")),
                     h3("Model Plots"),
-                    plotOutput(ns("model_plots"))
+                    plotOutput(ns("model_plots")),
+                    verbatimTextOutput(ns("debug"))
                 )
             )
         )
@@ -67,7 +66,7 @@ regression_ui <- function(id) {
 
 regression_server <- function(
     input, output, session,
-    file_input) {
+    file_input, digits) {
     # Create UI for predictor blocks based on number of blocks
     output$predictor_blocks <- renderUI({
         # Create a list of UI elements
@@ -108,6 +107,192 @@ regression_server <- function(
         }
     })
 
+    # empty_predictors is TRUE if any of the input$block_i is length 0
+    empty_predictors <- reactive({
+        empty_predictors <- TRUE
+        for (i in 1:input$blocks) {
+            if (length(input[[paste0("block_", i)]]) == 0) {
+                empty_predictors <- TRUE
+            } else {
+                empty_predictors <- FALSE
+            }
+        }
+        return(empty_predictors)
+    })
+
+    # Predictors
+    predictors <- reactive({
+        predictors <- list()
+        for (i in 1:input$blocks) {
+            predictors[[i]] <- input[[paste0("block_", i)]]
+        }
+        return(predictors)
+    })
+
+    # Data frame
+    df <- reactive({
+        return(data.frame(
+            file_input$df %>%
+                select(input$outcome, unlist(predictors()))
+        ))
+    })
+
+    # Model
+    #  Only if no block is empty
+    models <- reactive({
+        if (empty_predictors() == FALSE) {
+            # Create a list of models
+            models <- list()
+            # If predictors is not empty, build a model for each block
+            for (i in 1:input$blocks) {
+                models[[i]] <- lm(
+                    formula = as.formula(paste0(
+                        input$outcome, " ~ ",
+                        paste0(
+                            # Predictors are cumulative
+                            unlist(predictors()[1:i]),
+                            collapse = " + "
+                        )
+                    )),
+                    data = df()
+                )
+            }
+            # Standardize models
+            models[[i]] <- lm.beta(models[[i]])
+            # Return a list of models
+            return(models)
+        }
+    })
+
+    # Model ANOVA
+    anova_stat <- reactive({
+        # Create a list of anova tables
+        anova_tables <- list()
+        # Compare each model to the previous model
+        if (length(models()) > 1) {
+            for (i in 2:length(models())) {
+                anova_tables[[i]] <- anova(
+                    models()[[i - 1]],
+                    models()[[i]]
+                )
+            }
+        }
+        # Return a list of anova tables
+        return(anova_tables)
+    })
+
+    # Model fit measures for each block in a table. Each row is a model.
+    output$model_fit_measures <- renderTable(
+        {
+            # Create a list of model fit measures
+            model_fit_measures <- data.frame()
+
+            # If models length is not null, build a model for each block
+            if (length(models()) > 0) {
+                for (i in 1:length(models())) {
+                    # Build model for each block
+                    model_fit_measures <- rbind(
+                        model_fit_measures,
+                        data.frame(
+                            model = paste0(i),
+                            r = sqrt(summary(models()[[i]])$r.squared),
+                            r.squared = summary(models()[[i]])$r.squared,
+                            adj.r.squared = summary(
+                                models()[[i]]
+                            )$adj.r.squared,
+                            AIC = AIC(models()[[i]]),
+                            BIC = BIC(models()[[i]]),
+                            RMSE = sqrt(mean(models()[[i]]$residuals^2)),
+                            fstat = summary(models()[[i]])$fstatistic[1],
+                            df1 = summary(models()[[i]])$fstatistic[2],
+                            df2 = summary(models()[[i]])$fstatistic[3],
+                            p.value = pf(
+                                summary(models()[[i]])$fstatistic[1],
+                                summary(models()[[i]])$fstatistic[2],
+                                summary(models()[[i]])$fstatistic[3],
+                                lower.tail = FALSE
+                            ),
+                            stringsAsFactors = FALSE
+                        )
+                    )
+                }
+
+                # Return models
+                colnames(model_fit_measures) <- c(
+                    "Model", "R", "R²", "Adj. R²", "AIC", "BIC", "RMSE",
+                    "F", "df1", "df2", "p-value"
+                )
+                return(model_fit_measures)
+            }
+        },
+        digits = digits
+    )
+    # Model change table
+    output$model_change_measures <- renderTable(
+        {
+            # Create a list of model change measures
+            model_change_measures <- data.frame()
+
+            # If models length is not null, build a model for each block
+            if (length(models()) > 0) {
+                for (i in 1:length(models())) {
+                    # Build model for each block
+                    model_change_measures <- rbind(
+                        model_change_measures,
+                        data.frame(
+                            model = paste0(i - 1, " to ", i),
+                            r.squared = summary(models()[[i]])$r.squared,
+                            rsq.change = ifelse(
+                                length(models()) > 1 & i > 1,
+                                summary(models()[[i]])$r.squared -
+                                    summary(models()[[i - 1]])$r.squared,
+                                #  R squared for the first model
+                                summary(models()[[i]])$r.squared
+                            ),
+                            fstat.change = ifelse(
+                                length(models()) > 1 & i > 1,
+                                anova_stat()[[i]][2, 5],
+                                # F statistic for the first model
+                                summary(models()[[1]])$fstatistic[1]
+                            ),
+                            df1 = ifelse(
+                                length(models()) > 1 & i > 1,
+                                anova_stat()[[i]][2, 3],
+                                # F statistic for the first model
+                                summary(models()[[1]])$fstatistic[2]
+                            ),
+                            df2 = ifelse(
+                                length(models()) > 1 & i > 1,
+                                anova_stat()[[i]][2, 1],
+                                # F statistic for the first model
+                                summary(models()[[1]])$fstatistic[3]
+                            ),
+                            p.value = ifelse(
+                                length(models()) > 1 & i > 1,
+                                anova_stat()[[i]][2, 6],
+                                # F statistic for the first model
+                                pf(
+                                    summary(models()[[1]])$fstatistic[1],
+                                    summary(models()[[1]])$fstatistic[2],
+                                    summary(models()[[1]])$fstatistic[3],
+                                    lower.tail = FALSE
+                                )
+                            ),
+                            stringsAsFactors = FALSE
+                        )
+                    )
+                }
+
+                # Return models
+                colnames(model_change_measures) <- c(
+                    "Model", "R²", "ΔR²",
+                    "ΔF", "df1", "df2", "Δp-value"
+                )
+                return(model_change_measures)
+            }
+        },
+        digits = digits
+    )
     # Debug
     output$debug <- renderPrint({
         list()
