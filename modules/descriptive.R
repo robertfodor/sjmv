90f60e1869a35ec28371353eec07e2fe4e63de41
Róbert Fodor <fodor.robert@gmail.com>
2023-05-01 12:51:40 +0200

    added Kolmogorov-Smirnov test and redesigned the module structure

--- modules/descriptive.R
+++ modules/descriptive.R
@@ -4,6 +4,7 @@ library(shiny)
 library(dplyr) # for data manipulation
 library(ggplot2) # for plotting
 library(e1071) # skewness and kurtosis
+library(nortest) # Lilliefors test
 library(shinyWidgets) # for custom widgets
 library(knitr) # for kable
 library(kableExtra) # extra formatting for kable
@@ -49,7 +50,7 @@ descriptive_ui <- function(id) {
             choices = c(
               "SD", "Var", "Range", "IQR", "Min", "Max"
             ),
-            selected = c("SD", "IQR", "Min", "Max")
+            selected = c("SD", "IQR")
           )
         ),
         column(
@@ -57,22 +58,20 @@ descriptive_ui <- function(id) {
           awesomeCheckboxGroup(
             inputId = ns("normality"),
             label = "Normality",
-            choices = c("Shapiro-Wilk", "Shapiro-Wilk p-value", "Normal"),
-            selected = NULL
-          ),
-          materialSwitch(
-            inputId = ns("switch_columns_rows"),
-            label = "Show variables on top",
-            right = TRUE,
-            status = "info",
-            value = FALSE
+            choices = c(
+              "Shapiro-Wilk",
+              "Kolmogorov-Smirnov"
+            ),
+            selected = c("Shapiro-Wilk")
           )
         )
       ),
       fluidRow(
         column(
           width = 12,
-          tableOutput(ns("descriptives"))
+          tableOutput(ns("descriptives")),
+          h4("Normality tests"),
+          tableOutput(ns("normality_table"))
         )
       )
     )
@@ -102,14 +101,22 @@ descriptive_server <- function(
     digits
   )
 
-  # Descriptive statistics
-  output$descriptives <- function() { # If at least one variable is selected
+  # Data source
+  df <- reactive({
+    if (length(input$var) > 0) {
+      df <- file_input$df %>% select(input$var)
+      return(df)
+    }
+  })
+
+  # Descriptives
+  statistics <- reactive({
     if (length(input$var) > 0) {
       # df is the subset of the datafile based on the selected variables
-      df <- file_input$df %>% select(input$var)
+      df <- df()
       digits <- digits()
       # Calculate the descriptives
-      descriptives <- data.frame(
+      central_tendency <- data.frame(
         n = apply(df, 2, function(x) {
           length(x) - sum(is.na(x))
         }),
@@ -123,7 +130,32 @@ descriptive_server <- function(
         median = apply(df, 2, median),
         mode = apply(df, 2, function(x) {
           x[which.max(tabulate(match(x, x)))]
-        }),
+        })
+      ) %>%
+        dplyr::mutate_if(
+          is.numeric,
+          ~ sprintf(paste0("%.", digits, "f"), .)
+        )
+
+      colnames(central_tendency) <- c(
+        "N", "Missing", "Mean", "Mean SE",
+        "Median", "Mode"
+      )
+
+      skewness_kurtosis <- data.frame(
+        skewness = apply(df, 2, e1071::skewness, type = 2),
+        kurtosis = apply(df, 2, e1071::kurtosis, type = 2)
+      ) %>%
+        dplyr::mutate_if(
+          is.numeric,
+          ~ sprintf(paste0("%.", digits, "f"), .)
+        )
+
+      colnames(skewness_kurtosis) <- c(
+        "Skewness", "Kurtosis"
+      )
+
+      dispersion <- data.frame(
         sd = apply(df, 2, sd),
         var = apply(df, 2, var),
         range = apply(df, 2, function(x) {
@@ -133,7 +165,18 @@ descriptive_server <- function(
           quantile(x, probs = 0.75) - quantile(x, probs = 0.25)
         }),
         min = apply(df, 2, min),
-        max = apply(df, 2, max),
+        max = apply(df, 2, max)
+      ) %>%
+        dplyr::mutate_if(
+          is.numeric,
+          ~ sprintf(paste0("%.", digits, "f"), .)
+        )
+
+      colnames(dispersion) <- c(
+        "SD", "Var", "Range", "IQR", "Min", "Max"
+      )
+
+      normality <- data.frame(
         shapiro_wilk = apply(df, 2, function(x) {
           shapiro.test(x)$statistic
         }),
@@ -143,63 +186,120 @@ descriptive_server <- function(
           if (shapiro.test(x)$p.value < .001) {
             "<.001"
           } else {
-            round(shapiro.test(x)$p.value, 3)
+            sprintf(paste0("%.", digits, "f"), shapiro.test(x)$p.value)
           }
         }),
-        normal = apply(df, 2, function(x) {
-          if (shapiro.test(x)$p.value < .05) {
-            "FALSE"
+        ks = apply(df, 2, function(x) {
+          ks.test(x, "pnorm", exact = TRUE)$statistic
+        }),
+        ks_p = apply(df, 2, function(x) {
+          if (ks.test(x, "pnorm", exact = TRUE)$p.value < .001) {
+            "<.001"
           } else {
-            "TRUE"
+            sprintf(paste0("%.", digits, "f"), ks.test(x, "pnorm", exact = TRUE)$p.value)
           }
         }),
-        skewness = apply(df, 2, e1071::skewness, type = 2),
-        kurtosis = apply(df, 2, e1071::kurtosis, type = 2)
-      )
-
-      # Assign column names based on list
-      colnames(descriptives) <- c(
-        "N", "Missing", "Mean", "Mean SE",
-        "Median", "Mode", "SD", "Var",
-        "Range", "IQR", "Min", "Max",
-        "Shapiro-Wilk", "Shapiro-Wilk p-value", "Normal",
-        "Skewness", "Kurtosis"
-      )
-
-      # Filter the table based on the checkboxes using dplyr
-      subset_descriptives <- descriptives %>%
-        select(
-          c(
-            input$central_tendency,
-            input$dispersion,
-            input$skewness_kurtosis,
-            input$normality
-          )
-        ) %>%
+        ks_lillie_p = apply(df, 2, function(x) {
+          if (lillie.test(x)$p.value < .001) {
+            "<.001"
+          } else {
+            sprintf(paste0("%.", digits, "f"), lillie.test(x)$p.value)
+          }
+        })
+      ) %>%
         dplyr::mutate_if(
           is.numeric,
           ~ sprintf(paste0("%.", digits, "f"), .)
         )
 
-      # If materialSwitch is on, transpose the table
-      if (input$switch_columns_rows) {
-        subset_descriptives <- t(subset_descriptives)
-      } else {
-        subset_descriptives <- subset_descriptives
-      }
+      # Combine the tables
+      statistics <- tibble(
+        descr = cbind(central_tendency, dispersion, skewness_kurtosis),
+        normality = normality
+      )
 
-      subset_descriptives %>%
+      return(statistics)
+    }
+  })
+
+
+  # Table outputs for descriptive statistics
+  output$descriptives <- function() { # If at least one variable is selected
+    if (length(input$var) > 0) {
+      # Filter the table based on the checkboxes using dplyr
+      header_above_ct <- c("Central tendency" = length(input$central_tendency))
+      header_above_dp <- c("Dispersion" = length(input$dispersion))
+      header_above_sk <- c("Skewness and kurtosis" = length(input$skewness_kurtosis))
+
+      statistics()$descr %>%
+        select(all_of(
+          c(input$central_tendency, input$dispersion, input$skewness_kurtosis)
+        )) %>%
         # Format as table
         knitr::kable(
           "html",
           align = "c",
-          row.names = TRUE
+          row.names = TRUE,
+          caption = "Descriptive statistics"
         ) %>%
         kableExtra::kable_classic(
           full_width = FALSE,
           html_font = "inherit",
           position = "left"
+        ) %>%
+        kableExtra::add_header_above(
+          c(" " = 1, header_above_ct, header_above_dp, header_above_sk)
         )
+    }
+  }
+
+  # Table outputs for normality
+  output$normality_table <- function() { # If at least one variable is selected
+    if (length(input$var) > 0) {
+      selections <- c()
+      header_above_sw <- c()
+      header_above_ks <- c()
+      colname_sw <- c()
+      colname_ks <- c()
+      if ("Shapiro-Wilk" %in% input$normality) {
+        selections <- c(selections, "shapiro_wilk", "shapiro_wilk_p")
+        header_above_sw <- c("Shapiro-Wilk test" = 2)
+        colname_sw <- c("W", "p-value")
+      }
+      if ("Kolmogorov-Smirnov" %in% input$normality) {
+        selections <- c(selections, "ks", "ks_p", "ks_lillie_p")
+        header_above_ks <- c("Kolmogorov-Smirnov test" = 3)
+        colname_ks <- c("D", "p-value", "Lilliefors p-value")
+      }
+
+      # Filter the table based on the checkboxes using dplyr
+      subset_normality <- statistics()$normality %>%
+        select(all_of(selections))
+
+      subset_normality <- subset_normality %>%
+        # Format as table
+        knitr::kable(
+          "html",
+          align = "c",
+          row.names = TRUE,
+          col.names = c(colname_sw, colname_ks),
+          caption = "Tests for normality",
+          escape = TRUE
+        ) %>%
+        kableExtra::kable_classic(
+          full_width = FALSE,
+          html_font = "inherit",
+          position = "left"
+        ) %>%
+        kableExtra::add_header_above(
+          c(
+            " " = 1,
+            header_above_sw,
+            header_above_ks
+          )
+        )
+
+      subset_normality
     }
   }
 }
