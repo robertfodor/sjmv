1213c2536273fad76e03831a1e20ecf68c228289
Róbert Fodor <fodor.robert@gmail.com>
2023-05-05 15:47:18 +0200

    descriptives now have parent header

--- modules/descriptive.R
+++ modules/descriptive.R
@@ -9,9 +9,6 @@ library(shinyWidgets) # for custom widgets
 library(knitr) # for kable
 library(kableExtra) # extra formatting for kable
 
-# Source plots
-source("modules/plots.R")
-
 # Define UI
 descriptive_ui <- function(id) {
     ns <- NS(id)
@@ -76,7 +73,7 @@ descriptive_ui <- function(id) {
                     materialSwitch(
                         inputId = ns("variables_across"),
                         label = "Show variables across",
-                        value = FALSE,
+                        value = TRUE,
                         status = "info"
                     )
                 )
@@ -86,8 +83,7 @@ descriptive_ui <- function(id) {
                     width = 12,
                     h3("Results"),
                     h4("Descriptive statistics"),
-                    tableOutput(ns("descriptives")),
-                    uiOutput(ns("plots"))
+                    tableOutput(ns("descriptives"))
                 )
             )
         )
@@ -99,22 +95,28 @@ statistics <- tibble(
     filter = c(
         "N", "Missing",
         "Mean", "Std Error of Mean", rep("Mean CI", 2), "Median", "Mode",
-        rep("Skewness", 2), rep("Kurtosis", 2),
         "SD", "Variance", "Range", "IQR", "Min", "Max",
+        rep("Skewness", 2), rep("Kurtosis", 2),
         rep("Shapiro-Wilk", 2),
         rep("Kolmogorov-Smirnov", 2)
+    ),
+    parent_label = c(
+        rep("Sample", 2), rep("Central tendency", 6),
+        rep("Dispersion", 6),
+        rep("Skewness", 2), rep("Kurtosis", 2),
+        rep("Shapiro-Wilk test", 2), rep("Kolmogorov-Smirnov test", 2)
     ),
     label = c(
         "N", "Missing",
         "M", "SEM", "CI lower bound", "CI upper bound", "Median", "Mode",
-        "Skewness", "SE", "Kurtosis", "SE",
         "SD", "Variance", "Range", "IQR", "Min", "Max",
+        "Skewness", "SE", "Kurtosis", "SE",
         "W", "p",
         "D", "p (Lilliefors)"
     ),
     specialformat = c(
         rep("int", 2),
-        rep("float", 6 + 4 + 6),
+        rep("float", 6 + 6 + 4),
         "float", "p",
         "float", "p"
     ),
@@ -133,11 +135,6 @@ statistics <- tibble(
         },
         function(x) median(x, na.rm = TRUE),
         function(x) as.numeric(names(sort(-table(x)))[1]),
-        # Asymmetry
-        function(x) e1071::skewness(x, type = 2),
-        function(x) sd(x, na.rm = TRUE) / sqrt(length(x) - sum(is.na(x))),
-        function(x) e1071::kurtosis(x, type = 2),
-        function(x) sd(x, na.rm = TRUE) / sqrt(length(x) - sum(is.na(x))),
         # Dispersion
         function(x) sd(x, na.rm = TRUE),
         function(x) var(x, na.rm = TRUE),
@@ -145,6 +142,11 @@ statistics <- tibble(
         function(x) IQR(x, na.rm = TRUE),
         function(x) min(x, na.rm = TRUE),
         function(x) max(x, na.rm = TRUE),
+        # Asymmetry
+        function(x) e1071::skewness(x, type = 2),
+        function(x) sd(x, na.rm = TRUE) / sqrt(length(x) - sum(is.na(x))),
+        function(x) e1071::kurtosis(x, type = 2),
+        function(x) sd(x, na.rm = TRUE) / sqrt(length(x) - sum(is.na(x))),
         # Normality
         function(x) shapiro.test(x)$statistic[[1]],
         function(x) shapiro.test(x)$p.value,
@@ -182,16 +184,16 @@ statistics <- tibble(
 
 # Function to get back the parent labels from the statistics
 .get_parent_labels <- function(x) {
-    parent_labels <- character()
+    labels <- character()
     for (f in intersect(x, statistics$filter)) {
         idx <- which(statistics$filter == f)
         if (length(idx) > 1) {
-            parent_labels <- c(parent_labels, statistics$filter[idx])
+            labels <- c(labels, statistics$parent_label[idx])
         } else {
-            parent_labels <- c(parent_labels, statistics$filter[[idx]])
+            labels <- c(labels, statistics$parent_label[[idx]])
         }
     }
-    return(parent_labels)
+    return(labels)
 }
 
 # Function to get back the special formats from the statistics
@@ -264,9 +266,7 @@ statistics <- tibble(
     return(t(stats))
 }
 
-
-
-# Define the UI for the application
+# Define the server for the application
 descriptive_server <- function(input, output, session,
                                file_input, non_factor_variables,
                                digits, p_value_digits, ci_level) {
@@ -308,8 +308,8 @@ descriptive_server <- function(input, output, session,
         inputfilter <- c(
             input$sample,
             input$central_tendency,
-            input$asymmetry,
             input$dispersion,
+            input$asymmetry,
             input$normality
         )
 
@@ -323,6 +323,7 @@ descriptive_server <- function(input, output, session,
 
             # Get the labels
             labels <- .get_labels(inputfilter)
+            parent_labels <- .get_parent_labels(inputfilter)
             columnnames <- c()
 
             # Apply formatting
@@ -331,6 +332,13 @@ descriptive_server <- function(input, output, session,
             stats_df <- .format_statistics(stats_df,
                 filter = inputfilter, digits = settings$digits,
                 pdigits = settings$p_value_digits
+            )
+
+            # Create a vector of stats parent names and count them
+            grouping <- factor(
+                parent_labels,
+                levels = unique(parent_labels),
+                labels = unique(parent_labels)
             )
 
             if (input$variables_across) {
@@ -344,12 +352,12 @@ descriptive_server <- function(input, output, session,
                 columnnames <- c("Variables", labels)
             }
 
-            # Format table
-            stats_df %>%
+            # Format table and return
+            stats_df <- stats_df %>%
                 kable(
                     "html",
                     align = c("l", rep("c", length(stats_df) - 1)),
-                    caption = "Descriptive statistics",
+                    # caption = "Descriptive statistics",
                     # Must store variables in column due to duplicate row names
                     row.names = FALSE,
                     col.names = columnnames,
@@ -360,6 +368,25 @@ descriptive_server <- function(input, output, session,
                     html_font = "inherit",
                     position = "left"
                 )
+
+            if (input$variables_across) {
+                stats_df <- stats_df %>%
+                    # Group rows by stats parent
+                    kableExtra::group_rows(
+                        index = table(grouping),
+                        group_label = grouping
+                    )
+            } else {
+                header <- c(c(" " = 1), table(grouping))
+                stats_df <- stats_df %>%
+                    # Group columns by stats parent
+                    kableExtra::add_header_above(
+                        header = header
+                    )
+            }
+
+            # Return
+            stats_df
         }
     }
 }
--- modules/plots.R
+++ modules/plots.R
@@ -1,12 +1,5 @@
 library(ggplot2)
 
 # Function to create a box plot with mean line
-gen_boxplot <- function(data, x_var, y_var) {
-    plot <- ggplot(data, aes(x = {{ x_var }}, y = {{ y_var }})) +
-        geom_boxplot(fill = "#99d8c9", color = "black") +
-        stat_summary(fun.y = mean, geom = "point", shape = 23, size = 3, fill = "white", color = "black") +
-        labs(x = deparse(substitute(x_var)), y = deparse(substitute(y_var))) +
-        theme_minimal()
-
-    return(plot)
+.plot_box_chart_singlevar <- function(df, selection) {
 }
